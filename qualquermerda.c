df69c5439078d164be469ef993a025130475c423
Phe0 <pheofo@gmail.com>
2018-05-11 19:27:42 -0300

    Adding new functions

--- qualquermerda.c
+++ qualquermerda.c
@@ -3,6 +3,8 @@
 int main(int argc, char const *argv[]) {
   printf("Qualquer bosta\n");
   printf("seila");
+  printf("AAAAAAAAAAAAAAAAAA");
+  printf("oie");
   printf("Olá mundo!\n", );
   return 0;
 }
